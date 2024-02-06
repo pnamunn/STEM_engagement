b78334d1bd688c71f15ec69f49cca4876ce194c2
Patricia Munn <104117292+pnamunn@users.noreply.github.com>
2024-02-05 23:37:49 -0800

    Update sketch.ino

--- JUSTIN_SPEC/sketch.ino
+++ JUSTIN_SPEC/sketch.ino
@@ -11,6 +11,8 @@ char motor_speed = 128;
 void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 
   analogWrite(enA, motor_speed);
   analogWrite(enB, motor_speed);
@@ -23,42 +25,42 @@ void loop() {
     char serial_input = Serial.read();
 
     switch (serial_input) {
-      case 'w':
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        break;
-      case 's':
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        break;
-      case 'a':
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        break;
-      case 'd':
+      case 'w':    // Forward
         digitalWrite(in1, HIGH);
         digitalWrite(in2, LOW);
         digitalWrite(in3, HIGH);
         digitalWrite(in4, LOW);
         break;
-      case 'q':
+      case 's':    // Backward
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        break;
+      case 'a':    // Left
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in4, LOW);
+        break;
+      case 'd':    // Right
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in3, LOW);
+        digitalWrite(in4, HIGH);
+        break;
+      case 'q':    // Stop
         digitalWrite(in1, LOW);
         digitalWrite(in2, LOW);
         digitalWrite(in3, LOW);
         digitalWrite(in4, LOW);
         break;
-      case 'z':
+      case 'z':    // Speed up
         motor_speed += 10;
         analogWrite(enA, motor_speed);
         analogWrite(enB, motor_speed);
         break;
-      case 'x':
+      case 'x':    // Slow down
         motor_speed -= 10;
         analogWrite(enA, motor_speed);
         analogWrite(enB, motor_speed);
